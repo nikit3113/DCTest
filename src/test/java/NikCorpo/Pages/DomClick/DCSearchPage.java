2ea032e1a1931d7eb7fbc264093ab9183ddc4de9
NikitKa <shukalin3891@mail.ru>
2018-08-05 13:56:59 +0300

    Add Search page in project, add min and max price elements

--- src/test/java/NikCorpo/Pages/DomClick/DCSearchPage.java
+++ src/test/java/NikCorpo/Pages/DomClick/DCSearchPage.java
@@ -1,7 +1,24 @@
 package NikCorpo.Pages.DomClick;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import ru.sbtqa.tag.pagefactory.WebPage;
+import ru.sbtqa.tag.pagefactory.annotations.ElementTitle;
 import ru.sbtqa.tag.pagefactory.annotations.PageEntry;
 
 @PageEntry(title = "Страница поиска")
-public class DCSearchPage {
+public class DCSearchPage extends WebPage {
+
+    @ElementTitle("Минимальная цена")
+    @FindBy(xpath = "//div[contains(@class, 'double-inputs')]/input[@name='price__gte']")
+    public WebElement minPrice;
+
+    @ElementTitle("Максимальная цена")
+    @FindBy(xpath = "//div[contains(@class, 'double-inputs')]/input[@name='price__lte']")
+    public WebElement maxPrice;
+
+    public DCSearchPage(WebDriver driver) {super(driver);
+    }
+
 }
