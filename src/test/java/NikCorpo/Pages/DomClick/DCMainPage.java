65840ee8ecf0553b08af39162b2459ac26c713fa
DESKTOP-0R5KRVH\nikit_m <nikita@diniska.studio>
2018-08-08 17:38:00 +0300

    increased timeout. add method for choise city

--- src/test/java/NikCorpo/Pages/DomClick/DCMainPage.java
+++ src/test/java/NikCorpo/Pages/DomClick/DCMainPage.java
@@ -4,9 +4,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import ru.sbtqa.tag.pagefactory.WebPage;
+import ru.sbtqa.tag.pagefactory.annotations.ActionTitle;
 import ru.sbtqa.tag.pagefactory.annotations.ElementTitle;
 import ru.sbtqa.tag.pagefactory.annotations.PageEntry;
 import ru.sbtqa.tag.pagefactory.context.PageContext;
+import ru.sbtqa.tag.pagefactory.utils.ExpectedConditionsUtils;
 
 @PageEntry(title = "ДомКлик — cервис для поиска и покупки недвижимости в ипотеку")
 public class DCMainPage extends WebPage {
@@ -26,5 +28,13 @@ public class DCMainPage extends WebPage {
     public DCMainPage(WebDriver driver) {
         super(driver);
         PageContext.setCurrentPage(this);
+    }
+
+    @ActionTitle("вводит название города")
+    public void enterCityName(String cityName) throws InterruptedException {
+        btnSecondary.click();
+        ExpectedConditionsUtils.waitUntilElementPresent(searchString);
+        searchString.sendKeys(cityName);
+        searchString.submit();
     }
 }
